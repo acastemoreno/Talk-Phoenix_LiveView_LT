8775e7e1ee489ff16a6c6bd6c370b187081ea309
Albert <acastemoreno@gmail.com>
2020-05-26 23:40:57 -0500

    slides finish

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Phoenix LiveView', 
+     subtitle: 'Real Time Apps. No need to write Javascript', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/phoenix.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Albert Castellano Moreno', 
+     company: 'Codeable', 
+     gplus: '', 
+     twitter: '', 
+     www: '', 
+     github: 'http://github.com/acastemoreno', 
+   }]
+};
